ed91f9414ae0f065bc48b8d243ed4806d6f62d20
kyoka2424 <kyoka.n.11526@gmail.com>
2024-10-16 22:41:48 +0900

    課題再提出

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,13 +1,13 @@
-console.log(1);
+
 let num = Math.floor(Math.random() * 100) + 1;
-if (num % 3 ===0) {
-  console.log("３の倍数です");
+if (num % 15 ===0) {
+  console.log("３と５の倍数です");
 }
 else if (num % 5 ===0) {
   console.log("５の倍数です");
 }
-else if (num % 15 ===0) {
-  console.log("3と5の倍数です");
+else if (num % 3 ===0) {
+  console.log("3の倍数です");
 }
 else{
   console.log(num);
