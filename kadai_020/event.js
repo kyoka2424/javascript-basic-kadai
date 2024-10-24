89f4e0cc4233dcd97a685f416dc2b7f63f4d2449
kyoka2424 <kyoka.n.11526@gmail.com>
2024-10-24 09:27:01 +0900

    課題提出

---
+++ kadai_020/event.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('btn');
+const text = document.getElementById('text')
+const addtext = document.getElementById('addtext')
+
+btn.addEventListener('click',() => {
+
+text.style.display = 'none';
+addtext.style.display = 'block';
+})
